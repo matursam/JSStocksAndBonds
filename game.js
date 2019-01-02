2abbdbe3b1d191286935f524ae9df915d7971a33
matursam <matursam@ncsu.edu>
2019-01-02 00:39:26 -0500

    Finished game functionality. Still some bugs and a few minor features to add. GUI is rough

--- game.js
+++ game.js
@@ -11,7 +11,7 @@ const bearMarket = [
 	[11, 11, -5, -7, 30, 10, -11, -18, -4],
 	[-5, 13, -8, 6, 25, 4, 18, -22, -4],
 	[-8, -10, -10, -15, -20, -20, -23, -25, -7]
-]
+];
 
 const bullMarket = [
 	[-2, -10, -7, -9, -2, -9, -7, -16, -4],
@@ -25,24 +25,87 @@ const bullMarket = [
 	[13, 31, 1, 14, -11, 18, 18, -14, 10],
 	[14, -8, 19, -1, -9, 25, -10, 13, 19],
 	[24, 24, 23, 20, 51, 27, 38, 33, 18],
-]
+];
 
-var stockValues = new Array(10)
+const dividends = [1.05, 1.01, 1.0, 1.04, 1.07, 1.0, 1.0, 1.02, 1.06, 1.03];
 
 //Define constants
-const NUM_TURNS = 10
+const NUM_TURNS = 10;
 const STARTING_STOCK_PRICE = 100;
+const STARTING_CASH = 5000;
 //Define global variables
 var turnNumber = 0;
-var currentMarket = "Bull"
+var currentMarket = "Bull";
+// Create an array to store the stock values
+var stockValues = new Array(10);
+
+// Create an array to store the amount of each stock held by the player
+var holdings = new Array(10);
+
+// Create a variable to store the current amount of cash for the user
+var cash;
 
 function startNewGame() {
+	// Reset the board and all array values
+	resetBoard();
+	
+	document.getElementById("turn number").innerHTML = "Turn: " + turnNumber;
+	currentMarket = bullOrBear();
+	var calculator;
+	if(currentMarket == "Bull") {
+		calculator = bullMarket;
+	}
+	else {
+		calculator = bearMarket;
+	}
+	//Roll dice to get market value
+	var calculatorIndex = diceRoll() - 2;
+	
+	//Calculate initial stock values
+	console.log("Calulating Stock Prices:");
+	for(i = 1; i < 10; i++) {
+		var price = STARTING_STOCK_PRICE + calculator[calculatorIndex][i - 1];
+		stockValues[i][turnNumber] = String(price);
+		console.log(price);
+	}
+	//Update the board
+	updateBoard();
+}
+
+function resetBoard() {
+	// Reset the holdings array
+	holdings = new Array(10);
+	for(i = 0; i < 10; i++) {
+		holdings[i] = 0;
+	}
 	// Initialize the stock values inner arrays
 	for(i = 0; i < 10; i++) {
 		stockValues[i] = new Array(10);
 	}
+	// Clear the stock display board
+	for(i = 0; i <= 11; i++) {
+		stockValues[0][i] = 1000; 
+	}
+	for(i = 1; i <= 10; i++) {
+		for(j = 1; j <= 11; j++) {
+			document.getElementById(i + "-" + j).innerHTML = "   ";
+		}
+	}
 	turnNumber = 1;
-	document.getElementById("turn number").innerHTML = "Turn: " + turnNumber;
+	cash = 5000;
+}
+
+function nextTurn() {
+	turnNumber++;
+	if(turnNumber > 11) {
+		return;
+	}
+	if(turnNumber == 11) {
+		document.getElementById("turn number").innerHTML = "Game Finished";
+	}
+	else {
+		document.getElementById("turn number").innerHTML = "Turn: " + turnNumber;
+	}
 	currentMarket = bullOrBear();
 	var calculator;
 	if(currentMarket == "Bull") {
@@ -56,8 +119,17 @@ function startNewGame() {
 	
 	//Calculate initial stock values
 	console.log("Calulating Stock Prices:")
-	for(i = 1; i < 9; i++) {
-		var price = STARTING_STOCK_PRICE + calculator[calculatorIndex][i];
+	// For every stock, calculate the new value
+	for(i = 1; i < 10; i++) {
+		// Get the last stock price 
+		var lastPrice = Number(stockValues[i][turnNumber - 1]);
+		var price = lastPrice + Number(calculator[calculatorIndex][i - 1]);
+		if(price >= 150) {
+			// The stock has split, it's values need to be halved and shares doubled
+			price = String(price) + "/" + String(Math.ceil(price / 2));
+			// Double the number of holdings
+			holdings[i] *= 2;
+		}
 		stockValues[i][turnNumber] = String(price);
 		console.log(price);
 	}
@@ -65,19 +137,64 @@ function startNewGame() {
 	updateBoard();
 }
 
-function nextTurn() {
-	
+function buy(index, amount) {
+	// Get the price of the stock we're trying to buy
+	var price = stockValues[index][turnNumber] * amount;
+	console.log(price);
+	if(price > cash) {
+		// We can't afford to buy this stock, do nothing
+		return;
+	}
+	// We can afford to buy the stock, go ahead and buy it
+	cash -= price;
+	holdings[index] += amount;
+	var value = holdings[index] * stockValues[index][turnNumber];
+	// Update gui values
+	document.getElementById("H-" + index + "-0").innerHTML = holdings[index];
+	document.getElementById("cash").innerHTML = "Cash: $" + parseInt(cash);
+	document.getElementById("H-" + index + "-1").innerHTML = "$" + holdings[index] * stockValues[index][turnNumber];
+}
+
+function sell(index, amount) {
+	// Check if we have enough of the holding to sell
+	if(holdings[index] < amount) {
+		// We don't have enough shares to sell, do nothing
+		return;
+	}
+	// We can sell the requested amount, go ahead and sell
+	cash += amount * stockValues[index][turnNumber];
+	holdings[index] -= amount;
+	// Update gui values
+	document.getElementById("H-" + index + "-0").innerHTML = holdings[index];
+	document.getElementById("cash").innerHTML = "$" + parseInt(cash);
+	document.getElementById("H-" + index + "-1").innerHTML = "$" + holdings[index] * stockValues[index][turnNumber];
+}
+
+function price(index) {
+	return parseInt(stockValues[index][turnNumber], 10);
 }
 
 function updateBoard() {
 	console.log("Updating Board:")
 	//Set the bond price to 1000, since it never changes
 	document.getElementById("1-" + turnNumber).innerHTML = "1000";
-	for(i = 2; i < 10; i++) {
+	for(i = 2; i <= 10; i++) {
 		var id = i + "-" + turnNumber;
 		var current = document.getElementById(String(id));
 		current.innerHTML = String(stockValues[i - 1][turnNumber]);
 		console.log(stockValues[i - 1][turnNumber]);
+		if(String(stockValues[i - 1][turnNumber]).includes('/')) {
+			stockValues[i - 1][turnNumber] = String(stockValues[i - 1][turnNumber]).split('/')[1];
+		}
+		console.log("Updating " + i);
+		document.getElementById("H-" + Number(i-1) + "-0").innerHTML = holdings[i - 1];
+		document.getElementById("cash").innerHTML = "Cash: $" + parseInt(cash);
+		document.getElementById("H-" + Number(i-1) + "-1").innerHTML = "$" + (holdings[i - 1] * stockValues[i - 1][turnNumber]);
+	}
+	// Assign dividends
+	for(i = 0; i < 10; i++) {
+		cash += holdings[i] * dividends[i];
+		console.log(holdings[i] * dividends[i]);
 	}
 }
 
